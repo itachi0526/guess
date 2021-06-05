39c85658ea91916281593fc97a2f222b89599d49
Eric Haung <fiser0317@gmail.com>
2021-06-05 12:03:58 +0800

    choose start and end

--- guess.py
+++ guess.py
@@ -1,11 +1,17 @@
+start = input('請輸入你想要的開始值：')
+end = input('請輸入你想要的結束值：')
+start = int(start)
+end = int(end)
+
 import random
-r = random.randint(1, 100)
+r = random.randint(start, end)
 a = 0
+
 while True:
     num = input('猜個數字： ')
     num = int(num)
     a = a + 1
-    print('一共猜了', a, '次')
+    print('猜了', a, '次')
     if num == r:
         print('猜對了ㄛ！')
         break
